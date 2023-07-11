13d3498482214f056010266dde8f6210606e450e
danielfilemon <107655438+danielfilemon@users.noreply.github.com>
2023-07-11 18:18:27 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
  class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello, World! I am Daniel, nice to meet you!");
+        System.out.println("Hello, World! I am Daniel, nice to meet you! ");
     }
 }
